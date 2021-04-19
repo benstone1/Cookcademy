74eb303aef8b3e715c20c049363ac2a54c83f247
Ben Stone <benjamin.h.stone@gmail.com>
2021-04-19 15:01:42 -0400

    add list and color

---
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeData.swift
+//  Cookcademy
+//
+//  Created by Ben Stone on 4/19/21.
+//
+
+import Foundation
+
+class RecipeData: ObservableObject {
+    @Published var recipes = Recipe.testRecipes
+}
---
+++ Cookcademy/Views/RecipesListView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Cookcademy
+//
+//  Created by Ben Stone on 4/19/21.
+//
+
+import SwiftUI
+
+struct RecipesListView: View {
+    @StateObject var recipeData = RecipeData()
+    
+    let listBackgroundColor = AppColor.background
+    let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: Text("Destination for \(recipe.mainInformation.name)"))
+            }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle(navigationTitle)
+    }
+}
+
+extension RecipesListView {
+    var recipes: [Recipe] {
+        recipeData.recipes
+    }
+    
+    var navigationTitle: String {
+        "All Recipes"
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            RecipesListView()
+        }
+    }
+}

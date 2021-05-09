86f9a2310cacd6c1da2436bd70e545ab90b7729d
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-05-08 19:40:48 -0500

    Update RecipesListView.swift
    I was wrong - the `NavigationView` within `body` is unnecessary.

--- Cookcademy/Views/ExploreRecipes/RecipesListView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipesListView.swift
@@ -20,46 +20,44 @@ struct RecipesListView: View {
     private let listTextColor = AppColor.foreground
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(recipes) { recipe in
-                    NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
-                }
-                .listRowBackground(listBackgroundColor)
-                .foregroundColor(listTextColor)
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
-            .navigationTitle(navigationTitle)
-            .toolbar(content: {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        isPresenting = true
-                    }, label: {
-                        Image(systemName: "plus")
-                    })
-                }
-            })
-            .sheet(isPresented: $isPresenting, content: {
-                NavigationView {
-                    ModifyRecipeView(recipe: $newRecipe)                
-                        .toolbar(content: {
-                            ToolbarItem(placement: .cancellationAction) {
-                                Button("Dismiss") {
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle(navigationTitle)
+        .toolbar(content: {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    isPresenting = true
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+        })
+        .sheet(isPresented: $isPresenting, content: {
+            NavigationView {
+                ModifyRecipeView(recipe: $newRecipe)                
+                    .toolbar(content: {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Dismiss") {
+                                isPresenting = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            if newRecipe.isValid {
+                                Button("Add") {
+                                    recipeData.add(recipe: newRecipe)
                                     isPresenting = false
                                 }
                             }
-                            ToolbarItem(placement: .confirmationAction) {
-                                if newRecipe.isValid {
-                                    Button("Add") {
-                                        recipeData.add(recipe: newRecipe)
-                                        isPresenting = false
-                                    }
-                                }
-                            }
-                        })
-                        .navigationTitle("Add a New Recipe")
-                }
-            })
-        }
+                        }
+                    })
+                    .navigationTitle("Add a New Recipe")
+            }
+        })
     }
 }
 

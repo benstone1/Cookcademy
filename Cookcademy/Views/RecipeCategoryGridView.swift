1348803f29894a480c8b0a09fa69066b03ebafad
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-05-16 19:19:09 -0500

    Update RecipeCategoryGridView.swift
    - Remove `.capitalized` since the `category.rawValue` is already capitalized.
    - Was `struct RecipeCateogryGridView` typo - corrected to `struct RecipeCategoryGridView`

--- Cookcademy/Views/RecipeCategoryGridView.swift
+++ Cookcademy/Views/RecipeCategoryGridView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct RecipeCateogryGridView: View {
+struct RecipeCategoryGridView: View {
     @StateObject private var recipeData = RecipeData()
 
     var body: some View {
@@ -38,7 +38,7 @@ struct CategoryView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .opacity(0.35)
-            Text(category.rawValue.capitalized)
+            Text(category.rawValue)
                 .font(.title)
         }
     }

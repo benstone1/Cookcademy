3f50b3297951589e090a1fc6b5f677f55d009e87
Ben Stone <benjamin.h.stone@gmail.com>
2021-07-28 23:27:21 -0400

    Update RecipeDetailView.swift

--- Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
@@ -75,7 +75,7 @@ struct RecipeDetailView: View {
                             }
                         }
                     }
-                    .navigationTitle("Edit Ingredient")
+                    .navigationTitle("Edit Recipe")
             }
         }
     }
